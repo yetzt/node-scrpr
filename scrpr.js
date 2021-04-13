26f035f49b87521aa447cf5f93df38dc9b2a7f8c
yetzt <git@yetzt.me>
2021-04-13 15:00:37 +0200

    prevent reference pollution

--- scrpr.js
+++ scrpr.js
@@ -131,7 +131,7 @@ const scrpr = function(opts){
 								if (xsv === null) return next(new Error("xsv not available"));
 						
 								var result = [];
-								xsv(self.xsv_opts[opt.parse] ).on("data", function(record){
+								xsv({ ...self.xsv_opts[opt.parse] }).on("data", function(record){
 									result.push(record);
 								}).on("end", function(){
 									return next(null, result);
